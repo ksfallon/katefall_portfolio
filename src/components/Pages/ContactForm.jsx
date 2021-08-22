5d07fbfeb4a7b8f65b9b929486cf1b54ca30463d
ksfallon <fallon.katelin@gmail.com>
2021-08-22 15:32:29 -0400

    edited contact form to include only the inputs for first, last name, email and comments

--- src/components/Pages/ContactForm.jsx
+++ src/components/Pages/ContactForm.jsx
@@ -1,78 +1,45 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import me from '../../assets/Athens pic.jpeg'
+import { Button, Form, FormGroup, Label, Input, Card, CardImg, CardBody,
+  CardTitle, CardSubtitle, } from 'reactstrap';
 
 const Contact = (props) => {
   return (
-    <Form>
-      <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-        <Input type="select" name="select" id="exampleSelect">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelectMulti">Select Multiple</Label>
-        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleText">Text Area</Label>
-        <Input type="textarea" name="text" id="exampleText" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleFile">File</Label>
-        <Input type="file" name="file" id="exampleFile" />
-        <FormText color="muted">
-          This is some placeholder block-level help text for the above input.
-          It's a bit lighter and easily wraps to a new line.
-        </FormText>
-      </FormGroup>
-      <FormGroup tag="fieldset">
-        <legend>Radio Buttons</legend>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option one is this and that—be sure to include why it's great
-          </Label>
+  <div>
+    <header className="text-center mb-5">
+      <h2 className="text-uppercase lined">Lets Connect!</h2>
+    </header>
+    <Card className="w-50 mx-auto">
+      <CardBody className="align-center">
+        <CardTitle tag="h5" className="text-center">Just fill out this form</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-center">I will get back to you as soon as I can.</CardSubtitle>
+      <Form>
+        <FormGroup>
+          <Label for="firstName">First Name</Label>
+          <Input type="firstName" name="firstName" id="firstName" placeholder="Your First Name" />
         </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option two can be something else and selecting it will deselect option one
-          </Label>
+
+        <FormGroup>
+          <Label for="lastName">Last Name</Label>
+          <Input type="lastName" name="lastName" id="lastName" placeholder="Your Last Name" />
         </FormGroup>
-        <FormGroup check disabled>
-          <Label check>
-            <Input type="radio" name="radio1" disabled />{' '}
-            Option three is disabled
-          </Label>
+
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" placeholder="Your@Email.com" />
         </FormGroup>
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" />{' '}
-          Check me out
-        </Label>
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
+
+        <FormGroup>
+          <Label for="exampleText">Text Area</Label>
+          <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+
+        <Button className="btn btn-info">Submit</Button>
+      </Form>
+      </CardBody>
+      <CardImg className="image" width="50%" src={me} alt="Card image cap" />
+    </Card>
+  </div>
   );
 }
 
