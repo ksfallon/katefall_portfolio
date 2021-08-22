c25b4ebaa87f300c1800a4a11855f701c3b9678f
ksfallon <fallon.katelin@gmail.com>
2021-08-21 22:07:46 -0400

    aligning PortfolioPage to get rid of errors

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -4,7 +4,7 @@ import NavigationBar from './NavBar';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
-  
+
     // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
     const renderPage = () => {
     //   if (currentPage === 'Home') {
@@ -13,23 +13,23 @@ export default function PortfolioContainer() {
     //   if (currentPage === 'About') {
     //     return <About />;
     //   }
-      if (currentPage === 'Projects') {
+        if (currentPage === 'Projects') {
         return <Projects />;
-      }
+        }
     //   return <Contact />;
     };
-  
+
     const handlePageChange = (page) => setCurrentPage(page);
-  
+
     return (
-      <div>
-        {/* We are passing the currentPage from state and the function to update it */}
-        <NavigationBar currentPage={currentPage} handlePageChange={handlePageChange} />
-        {/* Here we are calling the renderPage method which will return a component  */}
-        {renderPage()}
-      </div>
+        <div>
+            {/* We are passing the currentPage from state and the function to update it */}
+            <NavigationBar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {/* Here we are calling the renderPage method which will return a component  */}
+            {renderPage()}
+        </div>
     );
-  }
+}
 
 
 
