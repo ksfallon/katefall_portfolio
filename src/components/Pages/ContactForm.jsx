cf66ffda66da8ef7fc1ccccfcdb6a110a6baebd0
ksfallon <fallon.katelin@gmail.com>
2021-08-22 20:36:23 -0400

    fixed some css bugs in contact form and projects

--- src/components/Pages/ContactForm.jsx
+++ src/components/Pages/ContactForm.jsx
@@ -8,39 +8,41 @@ import './ContactForm.css';
 const Contact = (props) => {
   return (
   <div>
-    <header className="text-center m-5">
-      <h2 className="titleContact text-uppercase lined" style={{fontFamily: 'Rampart One'}}>Lets Connect!</h2>
-    </header>
-    <Card className="cardContact w-50 mx-auto" style={{fontFamily: 'Fredoka One'}}>
-      <CardBody className="align-center">
-        <CardTitle tag="h5" className="intro text-center ">Just fill out this form</CardTitle>
-        <CardSubtitle tag="h6" className="introPlus mb-5 text-center">I will get back to you as soon as I can.</CardSubtitle>
-      <Form className="m-3">
-        <FormGroup className="m-3">
-          <Label for="firstName">First Name</Label>
-          <Input type="firstName" name="firstName" id="firstName" placeholder="Your First Name" />
-        </FormGroup>
+    <div className="container">
+      <header className="text-center m-5">
+        <h2 className="titleContact text-uppercase lined" style={{fontFamily: 'Rampart One'}}>Lets Connect!</h2>
+      </header>
+      <Card className="cardContact w-50 mx-auto" style={{fontFamily: 'Fredoka One'}}>
+        <CardBody className="cardContact align-center">
+          <CardTitle tag="h5" className="intro text-center mb-4">Just fill out this form</CardTitle>
+          <CardSubtitle tag="h6" className="introPlus mb-5 text-center">I will get back to you as soon as I can.</CardSubtitle>
+        <Form className="m-3">
+          <FormGroup className="m-3">
+            <Label for="firstName">First Name</Label>
+            <Input type="firstName" name="firstName" id="firstName" placeholder="Your First Name" />
+          </FormGroup>
 
-        <FormGroup className="m-3">
-          <Label for="lastName">Last Name</Label>
-          <Input type="lastName" name="lastName" id="lastName" placeholder="Your Last Name" />
-        </FormGroup>
+          <FormGroup className="m-3">
+            <Label for="lastName">Last Name</Label>
+            <Input type="lastName" name="lastName" id="lastName" placeholder="Your Last Name" />
+          </FormGroup>
 
-        <FormGroup className="m-3">
-          <Label for="email">Email</Label>
-          <Input type="email" name="email" id="email" placeholder="Your@Email.com" />
-        </FormGroup>
+          <FormGroup className="m-3">
+            <Label for="email">Email</Label>
+            <Input type="email" name="email" id="email" placeholder="Your@Email.com" />
+          </FormGroup>
 
-        <FormGroup className="m-3">
-          <Label for="exampleText">Additional Info</Label>
-          <Input type="textarea" name="text" id="exampleText" placeholder="Hi Katelin!, I am contacting you to ..." />
-        </FormGroup>
+          <FormGroup className="m-3">
+            <Label for="exampleText">Additional Info</Label>
+            <Input type="textarea" name="text" id="exampleText" placeholder="Hi Katelin!, I am contacting you to ..." />
+          </FormGroup>
 
-        <Button className="btn btn-info">Submit</Button>
-      </Form>
-      </CardBody>
-      {/* <CardImg className="image" width="50%" src={me} alt="Card image cap" /> */}
-    </Card>
+          <Button className="btn btn-info">Submit</Button>
+        </Form>
+        </CardBody>
+        {/* <CardImg className="image" width="50%" src={me} alt="Card image cap" /> */}
+      </Card>
+    </div>
   </div>
   );
 }
--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -17,24 +17,24 @@ const Projects = (props) => {
     <div className="d-flex h-100 align-items-center">
       <div className="container">
         <header className="text-center m-5">
-          <h1 className="title text-uppercase lined" style={{fontFamily: 'Rampart One'}}> Projects</h1>
+          <h1 className="titleProject text-uppercase lined" style={{fontFamily: 'Rampart One'}}> Projects</h1>
         </header>
         <div class="row justify-content-center pb-5">
 
           {/* 
           PROJECT 1
           */}
-          <Card inverse className="container expand col-md-6 col-lg-4 m-1">
+          <Card inverse className="cardProject expand col-md-6 col-lg-4 m-1 bg-dark">
             <CardImg id="image1" width="50%" src={food} alt="Card image cap" />
             <CardImgOverlay className="middle">
               <p className="divTitle">
                 <CardTitle className='title text-lg-center bg-info' tag="h3" style={{fontFamily: 'Rampart One'}}>Inspired Meals 4 U</CardTitle>
               </p>
-              <CardText className='text text-center' style={{fontFamily: 'Fredoka One'}}>Provides users a way to get personalized weekly meal plans based on each user’s: daily calorie goal, diet, and any foods to exclude. APIs: The spoonacular API Recipe – Food – Nutrition and Quotes Inspirational Quotes Motivational Quotes API.</CardText>
+              <CardText className='textProject text-center' style={{fontFamily: 'Fredoka One'}}>Provides users a way to get personalized weekly meal plans based on each user’s: daily calorie goal, diet, and any foods to exclude. APIs: The spoonacular API Recipe – Food – Nutrition and Quotes Inspirational Quotes Motivational Quotes API.</CardText>
               {/* <CardText>
                 <small className="text-muted">Languages Used: HTML5, CSS, JavaScript, Materialize, Foundation, jQuery, moment.js, Markdown</small>
               </CardText> */}
-              <div className="text-center">
+              <div className=" align-self-end text-center">
                 <Button className="text-center" color="info" href="https://github.com/ksfallon/Project-1" target="_blank" style={{fontFamily: 'Fredoka One'}}>GitHub {github}</Button>{' '}
                 <Button className="text-center" color="info" href="https://ksfallon.github.io/Project-1/" target="_blank" style={{fontFamily: 'Fredoka One'}}>Website {webEye}</Button>{' '}
               </div>
@@ -44,13 +44,13 @@ const Projects = (props) => {
           {/* 
           Work Day Scheduler
           */}
-          <Card inverse className="container expand col-md-6 col-lg-4 m-1 box-shadow">
+          <Card inverse className="cardProject expand col-md-6 col-lg-4 m-1 bg-dark">
             <CardImg id="image2" width="50%" src={calendar} alt="Card image cap" />
             <CardImgOverlay className="middle">
               <p className="divTitle">
                 <CardTitle className='title text-lg-center bg-info' tag="h3" style={{fontFamily: 'Rampart One'}}>Work Day Scheduler</CardTitle>
               </p>
-              <CardText className='text text-center'style={{fontFamily: 'Fredoka One'}}>Provides input blocks for each hour of the workday, 9am - 5pm, to allow the users to save notes for work events during specific times.</CardText>
+              <CardText className='textProject text-center'style={{fontFamily: 'Fredoka One'}}>Provides input blocks for each hour of the workday, 9am - 5pm, to allow the users to save notes for work events during specific times.</CardText>
               {/* <CardText>
                 <small className="text-muted">Languages Used: HTML5, CSS, JavaScript, Materialize, Foundation, jQuery, moment.js, Markdown</small>
               </CardText> */}
@@ -64,13 +64,13 @@ const Projects = (props) => {
           {/* 
           Weather Dashboard
           */}
-          <Card inverse className="container expand col-md-6 col-lg-4 m-1">
+          <Card inverse className="cardProject expand col-md-6 col-lg-4 m-1 bg-dark">
             <CardImg id="image3" width="50%" src={drops} alt="Card image cap" />
             <CardImgOverlay className="middle">
               <p className="divTitle">
                 <CardTitle className='title text-lg-center bg-info' tag="h3" style={{fontFamily: 'Rampart One'}}>Weather Dashboard</CardTitle>
               </p>
-              <CardText className='text text-center'style={{fontFamily: 'Fredoka One'}}>Uses the Open Weather Map APIs and moment.js to provide information about the current and five-day forecast based on the city search input.</CardText>
+              <CardText className='textProject text-center'style={{fontFamily: 'Fredoka One'}}>Uses the Open Weather Map APIs and moment.js to provide information about the current and five-day forecast based on the city search input.</CardText>
               {/* <CardText>
                 <small className="text-muted">Languages Used: HTML5, CSS, JavaScript, Materialize, Foundation, jQuery, moment.js, Markdown</small>
               </CardText> */}
